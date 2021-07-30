12f07b59b4e2ee3d21b1da126601dac61b3913d2
Angel <angeljcampo@gmail.com>
2021-07-30 17:58:49 -0400

    modifying test

--- spec/requests/wines_spec.rb
+++ spec/requests/wines_spec.rb
@@ -2,15 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Wines", type: :request do
   
-  describe 'GET wines#index' do
-    it 'should render wines#index template' do
-      @user = User.create(email: 'test@test.com', password: 'password123', password_confirmation: 'password123')
-      sign_in @user
-      get :index
-      expect(response).to render_template("index")
-    end
-  end
-
   describe 'You can"t view index without login' do
     it 'should redirect to sign_in view' do
       get :index
@@ -22,4 +13,13 @@ RSpec.describe "Wines", type: :request do
       expect(response).to redirect_to new_user_session_path
     end
   end
+  describe 'GET wines#index' do
+    it 'should render wines#index template' do
+      @user = User.create(email: 'test@example.com', password: '000000', password_confirmation: '000000')
+      sign_in @user
+      get :index
+      expect(response).to render_template("index")
+    end
+  end
+
 end
